c8c7e9e6450d17115fe7ea7c7282409ac555212a
DavidHarrisRid <david.rid4@gmail.com>
2026-07-09 10:18:10 +0200

    Initial commit

---
+++ AvaloniaJsonVisualization/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaJsonVisualization;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
